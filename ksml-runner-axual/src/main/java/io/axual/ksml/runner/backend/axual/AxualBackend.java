a719af6f6616d34e5e5369ffb8dfbf941b3ec308
Jeroen van Disseldorp <jeroen@axual.com>
2021-05-26 00:48:52 +0200

    Update AxualBackend logic

--- ksml-runner-axual/src/main/java/io/axual/ksml/runner/backend/axual/AxualBackend.java
+++ ksml-runner-axual/src/main/java/io/axual/ksml/runner/backend/axual/AxualBackend.java
@@ -9,9 +9,9 @@ package io.axual.ksml.runner.backend.axual;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,6 @@ package io.axual.ksml.runner.backend.axual;
  * limitations under the License.
  * =========================LICENSE_END==================================
  */
-
 
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -194,7 +193,7 @@ public class AxualBackend implements Backend {
             if (clusterSwitchDetected.getAndSet(false)) {
                 log.warn("Cluster switch detected, shutting down runner to reinitialize on new cluster");
                 axualStreams.stop();
-                while (axualStreams.state() != KafkaStreams.State.NOT_RUNNING || axualStreams.state() != KafkaStreams.State.ERROR) {
+                while (axualStreams.state() != KafkaStreams.State.NOT_RUNNING && axualStreams.state() != KafkaStreams.State.ERROR) {
                     // Check if runner is stopped
                     Utils.sleep(50);
                 }
@@ -205,7 +204,7 @@ public class AxualBackend implements Backend {
                 }
             }
 
-            final State state = getState();
+            final var state = getState();
             if (state == State.STOPPED || state == State.FAILED) {
                 log.info("Streams implementation has stopped, stopping Axual Backend");
                 break;
@@ -215,7 +214,7 @@ public class AxualBackend implements Backend {
     }
 
     public void discoveryPropertiesChanged(DiscoveryResult newDiscoveryResult) {
-        clusterSwitchDetected.set(discoveryResult != null);
+        clusterSwitchDetected.set(discoveryResult != null && newDiscoveryResult != null && !discoveryResult.equals(newDiscoveryResult));
         discoveryResult = newDiscoveryResult;
     }
 }
