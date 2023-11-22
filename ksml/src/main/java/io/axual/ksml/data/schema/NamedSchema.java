806ad6734c351683a3dc8d467fada295ae2af9d9
jeroenvandisseldorp <jeroen.van.disseldorp@axual.com>
2023-11-22 12:08:25 +0100

    Update compatibility checking between schema fields

--- ksml/src/main/java/io/axual/ksml/data/schema/NamedSchema.java
+++ ksml/src/main/java/io/axual/ksml/data/schema/NamedSchema.java
@@ -9,9 +9,9 @@ package io.axual.ksml.data.schema;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -56,10 +56,8 @@ public abstract class NamedSchema extends DataSchema {
     public boolean isAssignableFrom(DataSchema otherSchema) {
         if (!super.isAssignableFrom(otherSchema)) return false;
         if (!(otherSchema instanceof NamedSchema otherNamedSchema)) return false;
-        // Return true if the other named schema contains the same metadata
-        return (Objects.equals(namespace, otherNamedSchema.namespace)
-                && Objects.equals(name, otherNamedSchema.name));
-//                && Objects.equals(doc, otherNamedSchema.doc));
+        // Return true if the other named schema has the same name (ignore namespace and doc)
+        return Objects.equals(name, otherNamedSchema.name);
     }
 
     @Override
