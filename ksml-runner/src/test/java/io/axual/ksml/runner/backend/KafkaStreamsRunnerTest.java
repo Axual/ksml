3637b14eaf75db46e3aa18a3d2b6a9c9e8c9c90b
Kaspar <115068027+KasparMetsa@users.noreply.github.com>
2026-06-08 20:19:28 +0300

    Fix Sonar (#593)
    * Fix Sonar
    
    * Fix Sonar
    
    * Fix PR comments
    
    * Fix PR comments

--- ksml-runner/src/test/java/io/axual/ksml/runner/backend/KafkaStreamsRunnerTest.java
+++ ksml-runner/src/test/java/io/axual/ksml/runner/backend/KafkaStreamsRunnerTest.java
@@ -706,7 +706,9 @@ class KafkaStreamsRunnerTest {
             // Start runner, is blocked so check with async
             var future = CompletableFuture.runAsync(runner, executor);
 
-            verify(mockStreams, Mockito.timeout(1000).times(1)).start();
+            Awaitility.await("Wait for start() to be called")
+                    .atMost(Duration.ofSeconds(30))
+                    .untilAsserted(() -> verify(mockStreams, Mockito.times(1)).start());
             assertThat(future)
                     .as("Future should not have finished yet")
                     .isNotDone();
@@ -718,7 +720,7 @@ class KafkaStreamsRunnerTest {
             runner.stop();
 
             Awaitility.await("Wait for runner to stop")
-                    .atMost(Duration.ofSeconds(1))
+                    .atMost(Duration.ofSeconds(30))
                     .until(future::isDone);
         } finally {
             executor.shutdownNow();
